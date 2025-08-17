d7c1e69f836ad3d42a6a175b419be74511a7c3a5
Tamás Pogrányi <pogranyitamas99@gmail.com>
2025-08-17 12:29:06 +0200

    fix: theme

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,5 +1,6 @@
 import { AppBar, Toolbar, Typography, IconButton } from "@mui/material";
 import SettingsIcon from "@mui/icons-material/Settings";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 interface Props {
   onOpenConfig: () => void;
@@ -7,11 +8,21 @@ interface Props {
 
 export default function AppHeader({ onOpenConfig }: Props) {
   return (
-    <AppBar position="static" color="primary" elevation={1}>
+    <AppBar position="static" color="warning" elevation={1}>
       <Toolbar variant="dense">
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Jelenléti ív kitöltő
         </Typography>
+        <IconButton
+          color="inherit"
+          component="a"
+          href="https://github.com/AnsweRRR/jelenleti-iv-kitolto"
+          target="_blank"
+          rel="noopener noreferrer"
+          size="small"
+        >
+          <GitHubIcon />
+        </IconButton>
         <IconButton color="inherit" onClick={onOpenConfig} size="small">
           <SettingsIcon />
         </IconButton>
